a1a15ca61f68c7760bac5bbe8155376739bc6a39
HaoWang95 <alanwang0028@gmail.com>
2021-09-06 14:48:18 +1000

    Test scripts

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {render, unmountComponentAtNode} from 'react-dom'; // render function from react-dom
+import DatePicker from '../components/Date-picker.component';
+import SearchField from "../components/Search.component";
+import { act } from 'react-dom/test-utils';
+import { 
+  createRender 
+} from '@material-ui/core/test-utils'; // to test material ui component, must import material ui testing libs
+import {configure} from 'enzyme'; // 
+import Adapter from 'enzyme-adapter-react-16';
+import { create } from "react-test-renderer";
+
+configure({adapter: new Adapter()})
+
+let domContainer = null;
+beforeEach(() => {
+  // setup a dom element, I'm following the react doc
+  domContainer = document.createElement("div");
+  document.body.appendChild(domContainer);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(domContainer);
+  domContainer.remove();
+  domContainer = null;
+});
+
+it("Test <DatePicker /> component:", () => {
+  act(() => {
+    render(<DatePicker />, domContainer);
+  });
+  expect(domContainer);
+});
+
+describe("Test the <DatePicker />:",() => {
+  test("Run and render <DatePicker />", () => {
+    let tree = create(<DatePicker />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+});
+
+describe("Test the basic search field <SearchField />", () => {
+  test("Run and render <SearchField />", () => {
+    let tree = create(<SearchField />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+});
