aa71c2bccbf9ff82b508e45724f7c2fadcafec8c
HaoWang95 <alanwang0028@gmail.com>
2021-09-14 18:27:45 +1000

    adjust content layout

--- src/components/AppBar.component.jsx
+++ src/components/AppBar.component.jsx
@@ -29,7 +29,7 @@ import MailIcon from "@material-ui/icons/Mail";
 import { useState } from "react";
 import SearchField from "./Search.component";
 
-const drawerWidth = 220;
+const drawerWidth = 240;
 
 const useAppBarStyle = makeStyles((theme) => ({
   appBarRoot: {
@@ -48,8 +48,8 @@ const useAppBarStyle = makeStyles((theme) => ({
     width: `calc(100% - ${drawerWidth}px)`,
     //marginLeft: drawerWidth,
     transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
     }),
   },
   menuButton: {
@@ -61,7 +61,7 @@ const useAppBarStyle = makeStyles((theme) => ({
   },
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 1,
   },
   drawerPaper: {
     width: drawerWidth,
@@ -75,12 +75,13 @@ const useAppBarStyle = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(2),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: -drawerWidth,
+    marginLeft: drawerWidth/2,
+    marginRight: drawerWidth/2,
   },
 
   contentShift: {
@@ -88,7 +89,7 @@ const useAppBarStyle = makeStyles((theme) => ({
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    marginLeft: 0,
+    marginLeft: drawerWidth,
   },
   title: {
     flexGrow: 1,
@@ -196,7 +197,7 @@ const DefaultAppBar = () => {
       {renderAccountMenu}
       <Drawer
         className={style.drawer}
-        variant="temporary"
+        variant="persistent"
         anchor="left"
         open={drawerOpen}
         classes={{
@@ -225,7 +226,36 @@ const DefaultAppBar = () => {
           [style.contentShift]: drawerOpen,
         })}
       >
-        <div className={style.drawerHeader}></div>
+        <div className={style.drawerHeader} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
       </main>
     </div>
   );
