f99f7202d17af9421d8b6004d238d28fd60f1175
HaoWang95 <alanwang0028@gmail.com>
2021-09-15 11:52:46 +1000

    update: login and register form init

--- src/components/Register.component.jsx
+++ src/components/Register.component.jsx
@@ -1,6 +1,45 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Register(){
-    return (
-        <div>Register Component</div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  paper: {
+    margin: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+  },
+}));
+
+export default function Register() {
+  const style = useStyles();
+  return (
+    <Grid container spacing={3} className={style.root}>
+      <Paper className={style.paper}>
+        <FormControl>
+          <TextField
+            label="Email"
+            required
+            variant="outlined"
+            color="secondary"
+            spacing={3}
+          />
+          <TextField
+            label="Password"
+            required
+            variant="outlined"
+            color="secondary"
+            spacing={3}
+          />
+        </FormControl>
+      </Paper>
+    </Grid>
+  );
 }
